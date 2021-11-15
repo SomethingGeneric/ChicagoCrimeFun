b65c8c5c488abf8c852be2db0a7de54a7916ec30
Matt C <mdc028@bucknell.edu>
2021-11-14 22:49:29 -0500

    Co-authored-by: Ramon Asuncion  <asuncionbatista@gmail.com> Co-authored-by: XavierZZK <XavierZZK@users.noreply.github.com>

--- visualize.py
+++ visualize.py
@@ -1,49 +1,53 @@
-# stdlib
-import os
+import ete3
 
-# pip
-from flask import *
-
-# local
-from ChicagoCrime import ChicagoCrimeFun
-
-print("Loading data")
-ccf = ChicagoCrimeFun()
-
-app = Flask(__name__)
-
-if not os.path.exists("maps"):
-    print("Generating maps. Please wait")
-    ccf.map_all_types()
+from random import randint
 
 
-@app.route("/")
-def index():
-    html = """
-<html>
-<head>
-<title>Chicago Crime</title>
-</head>
-<body>
-<h1>Chicago Crime</h1>
-<ul>
-$STUFF$
-</li>
-</body>
-</html>    
-"""
-    insert = ""
-    for fn in os.listdir("maps"):
-        insert += "<li><a href='/map/" + fn + "'>" + fn + "</a></li>"
-    return html.replace("$STUFF$", insert)
+class visualizer:
+    def __init__(self, tree):
+        self.srctree = tree
+        self.tree = ete3.Tree()
+        self.dist = 0.5
+        self.support = 50
+
+        self.node_above = None
+
+    def draw_node(self, node):
+        pass
+
+    def draw(self, filename="output.png"):
+        self.tree.add_child(
+            name=self.srctree.root, dist=self.dist, support=self.support
+        )
+
+        self.draw_node(self.srctree.root.left)
+
+        self.tree.render(filename)
 
 
-@app.route("/map/<filename>")
-def map(filename):
-    if not os.path.exists("maps" + os.sep + filename):
-        abort(404)
-    return send_file("maps" + os.sep + filename)
+class SimpleNode:
+    def __init__(self, val=None):
+        self.left = None
+        self.right = None
+        self.val = val
+        print("My value is " + str(self.val))
+
+    def __repr__(self):
+        return str(self.val)
+
+
+class SimpleTree:
+    def __init__(self, root=None):
+        self.root = root
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    root = SimpleNode(randint(1, 100))
+    root.left = SimpleNode(randint(1, 100))
+    root.right = SimpleNode(randint(1, 100))
+
+    tree = SimpleTree(root)
+
+    v = visualizer(tree)
+    v.draw()
