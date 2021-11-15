2df37c54f410c1a0f210dde03fbe0978e8bfcabf
Matt C <mdc028@bucknell.edu>
2021-11-15 15:19:25 -0500

    metadata baseplate that i def didn't work on in class

--- ChicagoCrime.py
+++ ChicagoCrime.py
@@ -35,6 +35,8 @@ class ChicagoCrimeFun:
         self.location_tree = AVLTree()
         self.type_tree = AVLTree()
 
+        self.meta = None
+
         # https://docs.python.org/3/library/csv.html
         with open(TRAIN_FILE, newline="") as csvfile:
             csvreader = csv.reader(csvfile)  # read in the file, split it into a list.
@@ -62,6 +64,130 @@ class ChicagoCrimeFun:
         self.total_crimes -= 1
 
         print("Total data points: " + str(self.total_crimes))
+
+    def do_sort(self, x):
+        return {k: v for k, v in sorted(x.items(), key=lambda item: item[1])}
+
+    def make_metadata(self):
+        blocks = {}
+        iucr = {}
+        type_freq = {}
+        description = {}
+        location_desc = {}
+        arrest = {}
+        domestic = {}
+        beat = {}
+        district = {}
+        ward = {}
+        community_area = {}
+        fbi_code = {}
+
+        for case in self.cds:
+            # Block
+            if case.block not in blocks:
+                blocks[case.block] = 1
+            else:
+                blocks[case.block] += 1
+            
+            # IUCR (what's that?)
+            if case.iucr not in iucr:
+                iucr[case.iucr] = 1
+            else:
+                iucr[case.iucr] += 1
+
+            # Primary Type frequency
+            if case.primary_type not in type_freq:
+                type_freq[case.primary_type] = 1
+            else:
+                type_freq[case.primary_type] += 1
+
+            # Description frequency
+            if case.description not in description:
+                description[case.description] = 1
+            else:
+                description[case.description] += 1
+
+            # Location Description frequency
+            if case.location_description not in location_desc:
+                location_desc[case.location_description] = 1
+            else:
+                location_desc[case.location_description] += 1
+
+            # was there an arrest?
+            if case.arrest not in arrest:
+                arrest[case.arrest] = 1
+            else:
+                arrest[case.arrest] += 1
+
+            # was it domestic?
+            if case.domestic not in domestic:
+                domestic[case.domestic] = 1
+            else:
+                domestic[case.domestic] += 1
+
+            # beat (?)
+            if case.beat not in beat:
+                beat[case.beat] = 1
+            else:
+                beat[case.beat] += 1
+
+            # district
+            if case.district not in district:
+                district[case.district] = 1
+            else:
+                district[case.district] += 1
+
+            # ward
+            if case.ward not in ward:
+                ward[case.ward] = 1
+            else:
+                ward[case.ward] += 1
+
+            # community area
+            if case.community_area not in community_area:
+                community_area[case.community_area] = 1
+            else:
+                community_area[case.community_area] += 1
+
+            # fbi code
+            if case.fbi_code not in fbi_code:
+                fbi_code[case.fbi_code] = 1
+            else:
+                fbi_code[case.fbi_code] += 1
+
+            self.meta = {
+                "blocks": blocks,
+                "iucr": iucr,
+                "type_freq": type_freq,
+                "description": description,
+                "location_desc": location_desc,
+                "arrest": arrest,
+                "domestic": domestic,
+                "beat": beat,
+                "district": district,
+                "ward": ward,
+                "community_area": community_area,
+                "fbi_code": fbi_code
+            }
+
+    def print_meta(self, filename=None):
+        fobj = None
+        if filename is not None:
+            fobj = open(filename, "w")
+
+        if self.meta is None:
+            self.make_metadata()
+        for key in self.meta:
+            print("Data for " + key + ":\n-----\n")
+            if fobj is not None:
+                fobj.write("Data for " + key + ":\n-----\n")
+            print(str(self.meta[key]))
+            if fobj is not None:
+                fobj.write(str(self.meta[key]) + "\n")
+
+        if fobj is not None:
+            fobj.close()
+
 
     def build_loc_priority(self):
         """
@@ -145,10 +271,11 @@ if __name__ == "__main__":
     print("1 - Loading data")
     ccf = ChicagoCrimeFun()
     print("2 - Building location tree")
-    ccf.build_loc_priority()
+    #ccf.build_loc_priority()
     print("3 - Loading type priority tree")
-    ccf.build_crime_priority()
-    print("4 - Graphing type tree")
-    ccf.draw_this("type", "type.png")
-    print("5 - Graphing location tree")
-    ccf.draw_this("location", "location.png")
+    #ccf.build_crime_priority()
+    #print("4 - Graphing type tree")
+    #ccf.draw_this("type", "type.png")
+    #print("5 - Graphing location tree")
+    #ccf.draw_this("location", "location.png")
+    ccf.print_meta(input("Filename: "))
