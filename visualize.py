eab11999fc50fe41ea640c030e016cea5883e41c
Ramon Asuncion <asuncionbatista@gmail.com>
2021-12-01 12:02:38 -0500

    Add comments and update variable name.

--- visualize.py
+++ visualize.py
@@ -1,10 +1,9 @@
 from graphviz import Digraph
 
-class VisualizeData:
-    def __init__(self):
-        pass
-    def visualize_data(self, tree, dot=None, inital_call=True):
-        if inital_call:
+class VisualizeData:    
+    def visualize_data(self, tree, dot=None, initial_call=True):
+        # For first call use the root node. For subsequent calls use the node
+        if initial_call:
             tree = tree.root
         
         # Create Digraph object
@@ -12,15 +11,23 @@ class VisualizeData:
             dot = Digraph()
             dot.node(name=str(tree), label=str(tree.value), xlabel=str(tree.height))
 
-        # Add nodes
+        # Traverse through the left subtree.
         if tree.left:
+            # Add node to the graph
             dot.node(name=str(tree.left) , label=str(tree.left.value), xlabel=str(tree.left.height))
+            # Add the child node to the parent node
             dot.edge(str(tree), str(tree.left))
-            dot = self.visualize_data(tree.left, dot=dot, inital_call=False)
-            
+            # Recursive call through left subtree.
+            dot = self.visualize_data(tree.left, dot=dot, initial_call=False)
+        
+        # Traverse through the right subtree.  
         if tree.right:
+            # Add node to the graph
             dot.node(name=str(tree.right) ,label=str(tree.right.value), xlabel=str(tree.right.height))
+            # Add the child node to the parent node
             dot.edge(str(tree), str(tree.right))
-            dot = self.visualize_data(tree.right, dot=dot, inital_call=False)
+            # Recursive call through right subtree.
+            dot = self.visualize_data(tree.right, dot=dot, initial_call=False)
 
+        # Return the graph
         return dot
