038046ff2e92615b855001d95c0ba6cad49ba147
Ramon Asuncion <asuncionbatista@gmail.com>
2021-12-14 22:20:15 -0500

    Add comments to the visualize file.

--- visualize.py
+++ visualize.py
@@ -1,19 +1,19 @@
 from graphviz import Digraph
-
-"""
-Key: The data.
-Value: Priority Ranking.
-The visualizer shows the height of the tree on the top left of the node. 
-"""
-
-
-class VisualizeData:
-    def visualize_data(self, tree, dot=None, initial_call=True):
-        # For first call use the root node. For subsequent calls use the node
+class VisualizeData:    
+    def visualize_data(self, tree, dot=None, initial_call=True):    
+        """
+        Visualize funcion.
+        Tranverses the tree and creates a graph.
+            Arguments:
+            Arg1: [object] The binary tree.
+            Arg2: [object] The graph.
+            Arg3: [boolean] The first node is root. 
+        """
+        # For first call use the root node. For subsequent calls use the node.
         if initial_call:
             tree = tree.root
-
-        # Create Digraph object
+        
+        # Create Digraph object.
         if dot is None:
             dot = Digraph(strict=False)
             dot.node(name=str(tree), label=str(tree.key), xlabel=str(tree.height))
@@ -21,24 +21,16 @@ class VisualizeData:
         # Traverse through the left subtree.
         if tree.left:
             # Add node to the graph.
-            dot.node(
-                name=str(tree.left),
-                label=str(tree.left.key),
-                xlabel=str(tree.left.height),
-            )
+            dot.node(name=str(tree.left), label=str(tree.left.key), xlabel=str(tree.left.height))
             # Add the child node to the parent node.
             dot.edge(str(tree), str(tree.left))
             # Recursive call through left subtree.
             dot = self.visualize_data(tree.left, dot=dot, initial_call=False)
-
-        # Traverse through the right subtree.
+        
+        # Traverse through the right subtree.  
         if tree.right:
             # Add node to the graph.
-            dot.node(
-                name=str(tree.right),
-                label=str(tree.right.key),
-                xlabel=str(tree.right.height),
-            )
+            dot.node(name=str(tree.right), label=str(tree.right.key), xlabel=str(tree.right.height))
             # Add the child node to the parent node.
             dot.edge(str(tree), str(tree.right))
             # Recursive call through right subtree.
