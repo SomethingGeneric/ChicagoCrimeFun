9d0d07356d45d033dbf2e237bb76de9f801a5f69
Ramon Asuncion <asuncionbatista@gmail.com>
2023-02-19 06:03:27 -0500

    Add AVL and Heap tree visualization.

--- visualize.py
+++ visualize.py
@@ -1,40 +1,69 @@
 from graphviz import Digraph
 
-class VisualizeData:  
-    def visualize_data(self, node):
+class VisualizeData:
+    def visualize_avl(self, node):
         dot = Digraph()
-        return self._visualize_data(node.root, dot)
+        return self._visualize_avl(node.root, dot)
 
-    def _visualize_data(self, node, dot=None):    
+    def visualize_heap(self, heap):
+        dot = Digraph()
+        return self._visualize_heap(heap, dot)
+
+    def _visualize_heap(self, tree, dot=None, i=0):
         """
         Transverses the node and creates a graph.
             Arguments:
-            arg1: [object] The binary node.
+            arg1: [object] The node.
             arg2: [object] The graphviz object.
-            arg3: [boolean] The first node is root: True. 
-            
+
         Returns:
-            [object]: The graph (needed to be formatted).
-        """       
-        
+            [object]: The graph.
+        """
+
+        if dot is None:
+            dot = Digraph(strict=True)
+            dot.node(name=str(tree.heap[2 * i]), label='"' + str(tree.heap[2 * i]) + '"')
+
+        for i in range(1, tree.size+1):
+            if 2*i <= tree.size:
+                dot.node(name=str(tree.heap[i]), label=str(tree.heap[i]))
+                dot.edge(str(tree.heap[i//2]), str(tree.heap[i]))
+            if 2*i + 1 <= tree.size:
+                dot.node(name=str(tree.heap[2*i+1]), label=str(tree.heap[2*i+1]))
+                dot.edge(str(tree.heap[i]), str(tree.heap[2*i+1]))
+
+        return dot
+
+    def _visualize_avl(self, node, dot=None):
+        """
+        Transverses the node and creates a graph.
+            Arguments:
+            arg1: [object] The node.
+            arg2: [object] The graphviz object.
+
+        Returns:
+            [object]: The graph.
+        """
+
         # Create Digraph object.
         if node is not None:
-            # Add node to the graph.    
-            dot.node(name=str(node), label=str(node.key), xlabel=str(node.height))
+            # Add node to the graph.
+            dot.node(name=str(node), label=str(
+                node.key), xlabel=str(node.height))
 
             # Traverse through the left subnode.
             if node.left:
                 # Add node to the graph.
                 dot.edge(str(node), str(node.left))
                 # Recursive call through left subnode.
-                self._visualize_data(node.left, dot)
-            
-            # Traverse through the right subnode.  
+                self._visualize_avl(node.left, dot)
+
+            # Traverse through the right subnode.
             if node.right:
                 # Add node to the graph.
                 dot.edge(str(node), str(node.right))
                 # Recursive call through right subnode.
-                self._visualize_data(node.right, dot) 
+                self._visualize_avl(node.right, dot)
 
         # Return the graph.
         return dot
